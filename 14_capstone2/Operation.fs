3ae285d3e3e51f1708bb11b5b708d65c6c52346c
Patrick Drechsler <patrick@pdrechsler.de>
2017-11-27 20:43:56 +0100

    added missing auditAs function to Operation module

--- 14_capstone2/Operation.fs
+++ 14_capstone2/Operation.fs
@@ -1,5 +1,6 @@
 module Capstone2.Operations
 
+open System
 open Capstone2.Domain
 
 let deposit (amount: decimal) (account: Account) : Account =
@@ -8,3 +9,11 @@ let deposit (amount: decimal) (account: Account) : Account =
 let withdraw (amount: decimal) (account: Account) : Account =
     if amount > account.Balance then account
     else {  account with Balance = account.Balance - amount } 
+
+let auditAs operationName audit operation amount account =
+    audit account  (sprintf "%O: Perfoming a %s operation for %MEUR..." DateTime.UtcNow operationName amount)
+    let updatedAccount = operation amount account
+    let accountIsUnchanged = (updatedAccount = account)
+    if accountIsUnchanged then audit account (sprintf "%O: Transaction rejected!" DateTime.UtcNow)
+    else audit account (sprintf "%O: Transaction accepted! Balance is now %MEUR." DateTime.UtcNow updatedAccount.Balance)
+    updatedAccount
