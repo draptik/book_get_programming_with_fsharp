c39de7a7c7c56bae5dfa951115543d0f5206fe37
Patrick Drechsler <patrick@pdrechsler.de>
2017-11-27 20:59:39 +0100

    added fsx showcase for loading fs files in fsx

---
+++ 14_capstone2/ScratchpadIntegration.fsx
@@ -0,0 +1,21 @@
+#load "Domain.fs"
+#load "Operations.fs"
+#load "Auditing.fs"
+ 
+open Capstone2.Domain
+open Capstone2.Operations
+open Capstone2.Auditing
+open System
+ 
+let withdraw = withdraw |> auditAs "withdraw" console
+let deposit = deposit |> auditAs "deposit" console
+ 
+let customer = { Name = "Isaac" }
+let account = { AccountId = Guid.NewGuid(); Owner = customer; Balance = 90M }
+ 
+account
+|> withdraw 50M
+|> deposit 50M
+|> deposit 100M
+|> withdraw 50M
+|> withdraw 350M
