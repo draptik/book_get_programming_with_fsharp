0dd53ae67202cff3dbfd4932fb9c13da438fbdc1
Patrick Drechsler <patrick@pdrechsler.de>
2017-11-27 20:44:15 +0100

    added Main method content

--- 14_capstone2/Program.fs
+++ 14_capstone2/Program.fs
@@ -1,8 +1,43 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿module Capstone2.Program
 
 open System
+open Capstone2.Domain
+open Operations
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let mutable account =
+        let customer =
+            Console.Write "Please enter your name: "
+            let customerName = Console.ReadLine()
+            {Name = customerName}
+
+        Console.Write "Enter opening balance: "
+        let balance = Console.ReadLine() |> Decimal.Parse
+        {   AccountId = Guid.NewGuid()
+            Owner = customer
+            Balance = balance }
+
+
+    let withdrawWithAudit = withdraw |> auditAs "withdraw" Auditing.console
+    let depositWithAudit = deposit |> auditAs "deposit" Auditing.console
+
+    while true do
+        let action =
+            Console.WriteLine()
+            printfn "Current balance is %MEUR" account.Balance
+            Console.Write "(d)eposit, (w)ithdraw or e(x)it: "
+            Console.ReadLine()
+
+        if action = "x" then Environment.Exit 0
+
+        let amount =
+            Console.Write "Amount: "
+            Console.ReadLine() |> Decimal.Parse
+
+        account <-           
+        if action = "d" then account |> depositWithAudit amount
+        elif action = "w" then account |> withdrawWithAudit amount
+        else account
+    
     0 // return an integer exit code
