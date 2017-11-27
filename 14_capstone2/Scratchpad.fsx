a4ded8f4b73d17754ee03cf23e1c9c934e1383df
Patrick Drechsler <patrick@pdrechsler.de>
2017-11-27 19:03:32 +0100

    scratch: implemented fileSystemAudit and console logging

--- 14_capstone2/Scratchpad.fsx
+++ 14_capstone2/Scratchpad.fsx
@@ -1,4 +1,5 @@
 open System
+open System.IO
 
 type Customer = 
     { Name: string }
@@ -16,6 +17,7 @@ let withdraw (amount:decimal) (account:Account) : Account =
     if amount > account.Balance then account
     else {  account with Balance = account.Balance - amount } 
 
+// Test
 let bob = { Name = "Bob" }    
 let bobsId = Guid.NewGuid()
 
@@ -30,3 +32,22 @@ bobsAccount
 |> withdraw 2M
 |> withdraw 2000M
 // should return 28
+
+
+let fileSystemAudit (account: Account) (message: string) =
+    // target location: './logging/{customerName}/{accountId}.txt
+    // content format: "Account <accountId>: <message>"
+    // content example: "Account d89ac062-c777-4336-8192-6fba87920f3c: Performed operation 'withdraw' for 50EUR. Balance is now 75EUR"
+    // Linux path:
+    let basePath = "/home/patrick/projects/book_get_programming_with_fsharp/14_capstone2/logging"
+    Directory.CreateDirectory(sprintf "%s/%s" basePath account.Owner.Name) |> ignore
+    let filePath = sprintf "%s/%s/%O.txt" basePath account.Owner.Name account.AccountId
+    File.AppendAllLines(filePath, [ sprintf "Account %O: %s" account.AccountId message ])
+
+// Test
+fileSystemAudit bobsAccount "foo"
+
+let console account message =
+    printfn "Account %O: %s" account.AccountId message
+
+console bobsAccount "foo"    
