74d93963f3abf7198389610851bd470fb760b706
Patrick Drechsler <patrick.drechsler@redheads.de>
2017-11-29 09:19:27 +0100

    copied scratch code to Program (hardcoded values)

--- 19_capstone3/Program.fs
+++ 19_capstone3/Program.fs
@@ -1,8 +1,38 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open Capstone3.Domain
+open Capstone3.Operations
+
+let isValidCommand command =
+    let validCommands = ['d'; 'w'; 'x']
+    validCommands |> Seq.contains command
+
+let isStopCommand command =
+    'x' = command
+
+let getAmount (command:char) =
+    if command = 'd' then 'd', 50M
+    elif command = 'w' then 'w', 25M
+    else command, 0M
+
+let processCommand (account:Account) (command:char, amount:decimal) =
+    match command with
+    | 'd' -> deposit amount account 
+    | 'w' -> withdraw amount account
+    | _ -> account
+
+let openingAccount =
+        { Owner = { Name = "Isaac" }; Balance = 0M; AccountId = Guid.Empty }
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let commands = [ 'd'; 'w'; 'z'; 'f'; 'd'; 'x'; 'w' ]
+    let closingAccount =
+        commands
+        |> Seq.filter isValidCommand
+        |> Seq.takeWhile (not << isStopCommand)
+        |> Seq.map getAmount
+        |> Seq.fold processCommand openingAccount
+    printfn "%A" closingAccount
     0 // return an integer exit code
