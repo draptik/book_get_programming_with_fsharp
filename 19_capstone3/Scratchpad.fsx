d68f518681aaff278b1641503c347efe09e0ab61
Patrick Drechsler <patrick.drechsler@redheads.de>
2017-11-29 09:01:41 +0100

    implemented getAmount and processCommand

--- 19_capstone3/Scratchpad.fsx
+++ 19_capstone3/Scratchpad.fsx
@@ -1,5 +1,7 @@
 #load "Domain.fs"
+#load "Operations.fs"
 open Capstone3.Domain
+open Capstone3.Operations
 open System
 
 let openingAccount =
@@ -8,16 +10,26 @@ let openingAccount =
 let isValidCommand command =
     let validCommands = ['d'; 'w'; 'x']
     validCommands |> Seq.contains command
+// isValidCommand 'a'
+// isValidCommand 'd'
 
 let isStopCommand command =
     'x' = command
+// isStopCommand 'x'
 
-let getAmount command amount =
-    let validAmount = Decimal.Parse amount
-    (command, validAmount)
+let getAmount (command:char) =
+    if command = 'd' then 'd', 50M
+    elif command = 'w' then 'w', 25M
+    else command, 0M
 
-// let processCommand account commandAmountTuple =
-//     // TODO
+let processCommand (account:Account) (command:char, amount:decimal) =
+    match command with
+    | 'd' -> deposit amount account 
+    | 'w' -> withdraw amount account
+    | _ -> account
+// let acc1 = processCommand openingAccount ('d', 100M)
+// let acc2 = processCommand acc1 ('d', 100M)
+// let acc3 = processCommand acc2 ('w', 50M)
 
 let account =
     let commands = [ 'd'; 'w'; 'z'; 'f'; 'd'; 'x'; 'w' ]
