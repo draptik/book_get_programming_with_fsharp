2bbe363ef80dc759a9935390f65ee1fc8686a54d
Patrick Drechsler <patrick.drechsler@redheads.de>
2017-11-29 14:29:45 +0100

    fixed path in fileSystemAudit

--- 19_capstone3/Auditing.fs
+++ 19_capstone3/Auditing.fs
@@ -5,8 +5,7 @@ open System.IO
 open Capstone3.Domain.Transactions
 
 let fileSystemAudit (account: Account) (transaction: Transaction) =
-    // Linux path:
-    let basePath = "/home/patrick/projects/book_get_programming_with_fsharp/19_capstone3/logging"
+    let basePath = "logging"
     Directory.CreateDirectory(sprintf "%s/%s" basePath account.Owner.Name) |> ignore
     let filePath = sprintf "%s/%s/%O.txt" basePath account.Owner.Name account.AccountId
     File.AppendAllLines(filePath, [ sprintf "Account %O: %s" account.AccountId (transaction |> serialized) ])
