6b7a256ab408133d75ba1746cddff2d2dc3c9f69
Patrick Drechsler <patrick.drechsler@redheads.de>
2017-11-29 10:43:29 +0100

    auditing functions now take transaction as input (instead of string)

--- 19_capstone3/Auditing.fs
+++ 19_capstone3/Auditing.fs
@@ -2,14 +2,15 @@ module Capstone3.Auditing
 
 open Capstone3.Domain
 open System.IO
+open Capstone3.Domain.Transactions
 
-let fileSystemAudit (account: Account) (message: string) =
+let fileSystemAudit (account: Account) (transaction: Transaction) =
     // Linux path:
     let basePath = "/home/patrick/projects/book_get_programming_with_fsharp/19_capstone3/logging"
     Directory.CreateDirectory(sprintf "%s/%s" basePath account.Owner.Name) |> ignore
     let filePath = sprintf "%s/%s/%O.txt" basePath account.Owner.Name account.AccountId
-    File.AppendAllLines(filePath, [ sprintf "Account %O: %s" account.AccountId message ])
+    File.AppendAllLines(filePath, [ sprintf "Account %O: %s" account.AccountId (transaction |> serialized) ])
 
 
-let console (account: Account) (message: string) =
-    printfn "Account %O: %s" account.AccountId message
+let console (account: Account) (transaction: Transaction) =
+    printfn "Account %O: %s" account.AccountId (transaction |> serialized)
