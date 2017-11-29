99a4ffc2d8823d94edb20ae1f451c5cd913f5b0f
Patrick Drechsler <patrick.drechsler@redheads.de>
2017-11-29 14:30:50 +0100

    finished 19.3

--- 19_capstone3/Auditing.fs
+++ 19_capstone3/Auditing.fs
@@ -10,6 +10,5 @@ let fileSystemAudit (account: Account) (transaction: Transaction) =
     let filePath = sprintf "%s/%s/%O.txt" basePath account.Owner.Name account.AccountId
     File.AppendAllLines(filePath, [ sprintf "Account %O: %s" account.AccountId (transaction |> serialized) ])
 
-
 let console (account: Account) (transaction: Transaction) =
     printfn "Account %O: %s" account.AccountId (transaction |> serialized)
