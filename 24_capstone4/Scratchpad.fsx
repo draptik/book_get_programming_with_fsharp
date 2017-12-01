5ca9264f838696ce8be98c8cd162d422b78530cd
Patrick Drechsler <patrick.drechsler@redheads.de>
2017-12-01 14:54:30 +0100

    finished 21.2 creating/accessing discrimated unions

--- 24_capstone4/Scratchpad.fsx
+++ 24_capstone4/Scratchpad.fsx
@@ -149,3 +149,67 @@ let someCheck someList =
 genRandomNumbers 0 |> someCheck
 genRandomNumbers 10 |> someCheck
 5 :: genRandomNumbers 3 |> someCheck
+
+// 21 Modelling Relationships in F#
+
+// 21.1.1 Composition in F#
+
+type Disk1 = { SizeGb : int }
+type Computer =
+    { Manufacturer : string
+      Disks: Disk1 list }
+ 
+let myPc =
+    { Manufacturer = "Computers Inc."
+      Disks =
+        [ { SizeGb = 100 }
+          { SizeGb = 250 }
+          { SizeGb = 500 } ] }
+
+
+// 21.2 Discriminated Unions in F#          
+
+// (used instead of inheritance in OO)
+
+type Disk =
+| HardDisk of RPM:int * Platters:int
+| SolidState
+| MMC of NumberOfPins:int
+
+// 21.2.1 Creating instances of DUs
+
+let myHardDisk = HardDisk(RPM = 250, Platters = 7)
+let myHardDiskShort = HardDisk(250, 7)
+let args = 250, 7
+let myHardDiskTupled = HardDisk args
+let myMMC = MMC 5
+let mySsd = SolidState
+
+// 21.2.2 Accessing an instance of a DU
+
+let seek disk =
+    match disk with
+    | HardDisk _ -> "Seeking loudly at a reasonable speed!"
+    | MMC _ -> "Seeking quietly but slowly"
+    | SolidState -> "Already found it!"
+seek mySsd
+
+let describe disk =
+    match disk with
+    | SolidState -> "I'm a new-fangled SSD."
+    | MMC(1) -> "I've only got 1 pin."
+    | MMC(pins) when pins < 5 -> "I'm an MMC with a few pins"
+    | MMC(pins) -> sprintf "I'm an MMC with %i pins." pins
+    | HardDisk(5400, _) -> "I'm a slow hard disk"
+    | HardDisk(_, 7) -> "I have 7 spindles!"
+    | HardDisk _ -> "I'm a hard disk"
+
+SolidState |> describe
+MMC(1) |> describe
+MMC(2) |> describe
+MMC(10) |> describe
+HardDisk(5400, 10) |> describe
+HardDisk(5400, 7) |> describe
+HardDisk(1000, 7) |> describe
+HardDisk(1000, 1) |> describe
+
