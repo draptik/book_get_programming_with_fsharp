a143f8dd2b9fb54f184119e945e532e1ab5e9758
Patrick Drechsler <patrick@pdrechsler.de>
2017-12-03 17:56:20 +0100

    added scratchpad for chapter 23

---
+++ 24_capstone4/Scratchpad_23.fsx
@@ -0,0 +1,11 @@
+// 23 Business Rules as Code
+
+// 23.1 Specific types in F#
+
+type Customer =
+    {   CustomerId: string
+        Email: string
+        Telephone: string
+        Address: string }
+
+        
