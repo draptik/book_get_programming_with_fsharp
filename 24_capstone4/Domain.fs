e380f6bf844c983c318ddbcad873a92521400e27
Patrick Drechsler <patrick@pdrechsler.de>
2017-12-04 19:40:41 +0100

    reformated domain

--- 24_capstone4/Domain.fs
+++ 24_capstone4/Domain.fs
@@ -2,13 +2,9 @@ namespace Capstone4.Domain
 
 open System
 
-type Customer = 
-    { Name: string }
-
-type Account =
-    { Balance: decimal
-      AccountId: Guid
-      Owner: Customer }
+type Customer = { Name: string }
+type Account = { Balance: decimal; AccountId: Guid; Owner: Customer }
+type Transaction ={ Amount: decimal; Operation: string; Timestamp: DateTime; Accepted: bool }
 
 type CreditAccount = CreditAccount of Account
 
@@ -16,22 +12,12 @@ type RatedAccount =
     | Credit of CreditAccount
     | Overdrawn of Account
 
-type Transaction =
-    {   Amount: decimal
-        Operation: string
-        Timestamp: DateTime
-        Accepted: bool }
-
 type BankOperation = Deposit | Withdraw
 
 module Transactions =
     
     let serialized transaction =
-        sprintf "%O***%s***%M***%b"
-            transaction.Timestamp
-            transaction.Operation
-            transaction.Amount
-            transaction.Accepted
+        sprintf "%O***%s***%M***%b" transaction.Timestamp transaction.Operation transaction.Amount transaction.Accepted
 
     let deserialize (fileContent:string) =
         let parts = fileContent.Split([|"***"|], StringSplitOptions.None)
